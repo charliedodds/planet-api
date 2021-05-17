affde232d147f26426ba1a75a098d1ba094fffca
charliedodds <charlesgdodds@hotmail.co.uk>
2021-05-17 14:07:02 +0100

    Add put route

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.post('/planets', (req, res) => {
     planetKeys.includes('name', 'hasKnownLife', 'type', 'noOfMoons') &&
     planetToAdd.name &&
     planetToAdd.type &&
-    planetToAdd.noOfMoons
+    planetToAdd.noOfMoons >= 0
   ) {
     // attach new id to planet
     planetToAdd.id = newID;
@@ -60,6 +60,40 @@ app.post('/planets', (req, res) => {
     });
   } else {
     res.status(400).send({ message: 'Unable to create planet' });
+  }
+});
+
+app.put('/planets/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const indexToUpdate = planets.findIndex((planet) => planet.id === id);
+  if (indexToUpdate >= 0) {
+    let planetToUpdate = {};
+    if (typeof req.body.hasKnownLife === 'boolean') {
+      planetToUpdate = {
+        name: String(req.body.name),
+        hasKnownLife: req.body.hasKnownLife,
+        type: String(req.body.type),
+        noOfMoons: Number(req.body.noOfMoons),
+        // ...req.body,
+      };
+    }
+    const planetKeys = Object.keys(planetToUpdate);
+    if (
+      planetKeys.length === 4 &&
+      planetKeys.includes('name', 'hasKnownLife', 'type', 'noOfMoons', 'id') &&
+      planetToUpdate.name &&
+      planetToUpdate.type &&
+      planetToUpdate.noOfMoons >= 0
+    ) {
+      planetToUpdate.id = id;
+      planets.splice(indexToUpdate, 1, planetToUpdate);
+      res.status(200).send({
+        message: `Planet with id of ${id} updated`,
+        planet: planetToUpdate,
+      });
+    }
+  } else {
+    res.status(400).send({ message: 'Unable to update planet.' });
   }
 });
 
